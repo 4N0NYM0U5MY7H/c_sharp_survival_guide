4eaae9338f9c95386a63491379bb9f44191c9a9d
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 16:15:33 -0700

    Common Data Types

---
+++ Assets/Scripts/PlayerCommonDataTypes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCommonDataTypes : MonoBehaviour
+{
+    public int score;
+    public float speed = 5.5f;
+
+    public GameObject player;
+    public Animator anim;
+    public Transform myTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(myTransform.position.x);
+    }
+}
