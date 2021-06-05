bb7e36b31a33de3f757f74b480f4c4890343d110
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 14:55:36 -0700

    Destructible Crate

---
+++ Assets/Scripts/Crate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    public GameObject fracturedCrate;
+    public GameObject explostionEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(explostionEffect, transform.position, Quaternion.identity);
+
+            GameObject fracturedCrateObj = Instantiate(fracturedCrate, transform.position, Quaternion.identity);
+            Rigidbody[] allRigidBodies = fracturedCrateObj.GetComponentsInChildren<Rigidbody>();
+
+            if (allRigidBodies.Length > 0)
+            {
+                foreach (var body in allRigidBodies)
+                {
+                    body.AddExplosionForce(500, transform.position, 1);
+                }
+            }
+            
+            Destroy(this.gameObject);
+        }
+    }
+}
