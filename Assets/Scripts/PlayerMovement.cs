90fd971dd37e3d821049da5a443f8baba17b6c89
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 12:34:33 -0700

    Simple Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //             new Vector3(1, 0, 0) * 5 * real time
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * _speed * Time.deltaTime);
+    }
+}
