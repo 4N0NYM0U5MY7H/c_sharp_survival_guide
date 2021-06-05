957e30d58c952e463528ebc5eca07c3a2bae33fb
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 13:14:26 -0700

    Pause System

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
