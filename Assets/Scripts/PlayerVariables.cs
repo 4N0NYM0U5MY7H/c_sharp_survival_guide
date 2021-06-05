dd9e0822e06d1b84f3d5522d7cd6a8aa2cc72139
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 16:08:35 -0700

    Variables

---
+++ Assets/Scripts/PlayerVariables.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVariables : MonoBehaviour
+{
+    /// <summary>
+    /// public or private references
+    /// data type (strings, bools, float, int)
+    /// every variable needs a name
+    /// optional -- a value
+    /// </summary>
+
+    // variable for name
+    // variable for age
+    // varialbe for location
+
+    public string myName = "Jonathan";
+    public int myAge = 26;
+    public string myLocation = "Atlanta";
+    public bool hasKey = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myName = "Loren";
+        Debug.Log("My name is: " + myName);
+        Debug.Log("My age is: " + myAge);
+    }
+}
