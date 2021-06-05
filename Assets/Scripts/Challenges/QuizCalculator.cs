a16f0e3e4cce98a87ab317c93c3cf8697d92a1a4
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 16:58:15 -0700

    Challenge: Quiz Calculator

---
+++ Assets/Scripts/Challenges/QuizCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizCalculator : MonoBehaviour
+{
+    /// <summary>
+    /// five quiz grades
+    /// create a program taht calculates random quiz grades and prints the average
+    /// </summary>
+
+    public float quiz1, quiz2, quiz3, quiz4, quiz5;
+
+    // Use this for initialization
+    void Start()
+    {
+        quiz1 = Random.Range(0f, 100f);
+        quiz2 = Random.Range(0f, 100f);
+        quiz3 = Random.Range(0f, 100f);
+        quiz4 = Random.Range(0f, 100f);
+        quiz5 = Random.Range(0f, 100f);
+
+        float average = (quiz1 + quiz2 + quiz3 + quiz4 + quiz5) / 5;
+
+        average = Mathf.Round(average * 100f) / 100f;
+
+        Debug.Log("Average Quiz Score: " + average);
+    }
+}
