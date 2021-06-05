70ba8bbbd00573a4b1d8f670819d025ac2ec6348
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 12:10:31 -0700

    GameObject Position
    Move game objects with C# scripting

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 startPostion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // grab current pos = (assign) new position
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
