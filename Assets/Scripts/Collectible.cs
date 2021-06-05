1b07256d1cccace7f1408d425f442373d1cb20f4
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 12:49:16 -0700

    Collectible GameObjects

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // add points
+            // add powerup ability
+
+            Destroy(this.gameObject);
+        }
+    }
+}
