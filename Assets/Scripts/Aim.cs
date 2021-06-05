245e926030c677f8682f0e43d4be8e9b39dd498f
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 15:51:41 -0700

    Quaternion Look Rotation

---
+++ Assets/Scripts/Aim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _sphere;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // dicrection = destination - source
+        Vector3 directionToFace = _sphere.position - transform.position;
+        Debug.DrawRay(transform.position, directionToFace, Color.green);
+
+        // access current location = Quaternion Look rotation
+        transform.rotation = Quaternion.LookRotation(directionToFace);
+    }
+}
