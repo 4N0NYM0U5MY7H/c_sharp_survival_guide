32b3c879a5355330eb44d031e00d0f88bfde5659
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 00:04:45 -0700

    Looping Practical Example

---
+++ Assets/Scripts/Arrays/LoopingPractical.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingPractical : MonoBehaviour
+{
+    public GameObject[] cubes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var cube in cubes)
+            {
+                cube.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+    }
+}
