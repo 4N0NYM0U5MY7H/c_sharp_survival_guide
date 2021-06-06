e542cdf3689e7690d1a6406bec6a265dfec6592c
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 09:17:44 -0700

    Challenge: Are You Alive?

---
+++ Assets/Scripts/FunctionsVsMethods/PlayerStatus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    /// <summary>
+    /// Create a program that checks whether the player is dead or alive.
+    /// On space key press, damage the player by a random amount.
+    /// If the player is dead, print "The player has died!"
+    /// 
+    /// Bonus: Prevent the method from being called if the player is dead
+    ///        (no negative health values)
+    /// </summary>
+
+    public int health = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsDead() == false)
+        {
+            Damage(Random.Range(5, 20));
+        }
+    }
+
+    public void Damage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (IsDead())
+        {
+            health = 0;
+            Debug.Log("The player has died!");
+        }
+    }
+
+    public bool IsDead()
+    {
+        return health < 1;
+    }
+}
