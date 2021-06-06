2f1d061625ba4bdbb3662050c275a8ec162f3ca0
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 17:13:51 -0700

    Challenge: Master If Statements P3

---
+++ Assets/Scripts/Challenges/MasterIfStatementsP2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterIfStatementsP2 : MonoBehaviour
+{
+    /// <summary>
+    /// vairable to store points
+    /// if player presses space key
+    /// add 10 points
+    /// if points greater than or equal to 50
+    /// print you are awesome!
+    /// </summary>
+
+    [SerializeField]
+    private int _points;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _points += 10;
+        }
+
+        if (_points >= 50)
+        {
+            Debug.Log("You are awesome!");
+        }
+    }
+}
