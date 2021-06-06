13e0293edaeb4b67d86f8cea7c7a1d5d0b5b0a3f
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 19:47:28 -0700

    Weapon Select

---
+++ Assets/Scripts/Challenges/WeaponSelect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSelect : MonoBehaviour
+{
+    /// <summary>
+    /// id 1 = Gun
+    /// id 2 = Knife
+    /// id 3 = Machine Gun
+    /// </summary>
+
+    public int WeaponID;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (WeaponID)
+        {
+            case 1:
+                Debug.Log("Gun equipped");
+                break;
+            case 2:
+                Debug.Log("Knife equipped");
+                break;
+            case 3:
+                Debug.Log("Machine Gun equipped");
+                break;
+            default:
+                Debug.Log("You can't equip that");
+                break;
+        }
+    }
+}
