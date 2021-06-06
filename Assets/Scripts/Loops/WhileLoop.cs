16003a858661771c50196665c1f13a1163d0136b
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 22:06:13 -0700

    While Loops

---
+++ Assets/Scripts/Loops/WhileLoop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoop : MonoBehaviour
+{
+    public int apples;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (apples < 50)
+        {
+            Debug.Log(apples);
+            apples++;
+        }
+        Debug.Log("Loop has Finished");
+
+        StartCoroutine(LoopRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoopRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            Debug.Log("Spawning Enemy Every Second");
+        }
+    }
+}
