8e35c47963a1bddeb06fa4e6e410d98908bac243
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 22:52:36 -0700

    Access Array Elements

---
+++ Assets/Scripts/Arrays/AccessArrayElements.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessArrayElements : MonoBehaviour
+{    
+    // declare the size (how many elements) through the inspector
+    public string[] names;
+
+    public string[] items = new string[5];
+
+    public int[] ages = new int[] { 5, 3, 2, 55, 66 };
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(names[0]);
+        Debug.Log(ages[3]);
+        Debug.Log(items[1]);
+    }
+}
