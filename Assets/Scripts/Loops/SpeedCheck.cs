02841d2f5315e73a728c0266da4b747d7237eb54
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 22:31:40 -0700

    Challenge: How Fast Are You Going?

---
+++ Assets/Scripts/Loops/SpeedCheck.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedCheck : MonoBehaviour
+{
+    /// <summary>
+    /// Create a program that meets the following:
+    ///     increment the value of speed every second
+    ///     when speed is greater than MaxSpeed
+    ///         stop incrementing (Random between 60 and 120)
+    /// </summary>
+
+    public int speed;
+    public int maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxSpeed = Random.Range(60, 120);
+        StartCoroutine(SpeedRoutine());
+    }
+
+    IEnumerator SpeedRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            speed += 5;
+
+            if (speed > maxSpeed)
+            {
+                break;
+            }
+        }
+
+        /*
+        do
+        {
+            speed += 5;
+            yield return new WaitForSeconds(0.5f);
+
+        } while (speed > maxSpeed);*/
+    }
+}
