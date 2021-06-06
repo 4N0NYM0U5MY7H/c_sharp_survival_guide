9d73c33e07bb94bce1a8087d3f629f873b7b2ffb
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 15:25:08 -0700

    Classes for Behaviours

---
+++ Assets/Scripts/Classes/Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y > 30)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Classes/SpaceFighter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceFighter : MonoBehaviour
+{
+    public GameObject laserPrefab;
+    public Transform firePos;
+
+    [SerializeField]
+    private float _speed;
+    private float _xRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+        FireProjectile();
+    }
+
+    private void CalculateMovement()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(hInput, vInput, 0) * _speed * Time.deltaTime);
+
+        if (transform.position.x > _xRange)
+        {
+            transform.position = new Vector3(_xRange, transform.position.y, 0);
+        }
+        else if (transform.position.x < -_xRange)
+        {
+            transform.position = new Vector3(-_xRange, transform.position.y, 0);
+        }
+    }
+
+    void FireProjectile()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(laserPrefab, firePos.position, Quaternion.identity);
+        }
+    }
+}
