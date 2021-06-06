85f8da18e678217c3e4762630f75bb450305e9c1
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 17:21:07 -0700

    Challenge: Mastering If Statements P3

--- Assets/Scripts/Challenges/MasterIfStatementsP3.cs
+++ Assets/Scripts/Challenges/MasterIfStatementsP3.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MasterIfStatementsP2 : MonoBehaviour
+public class MasterIfStatementsP3 : MonoBehaviour
 {
     /// <summary>
     /// variable to store points
+    /// hasSaidMessage variable
     /// if player presses space key
     /// add 10 points
-    /// if points greater than or equal to 50
+    /// if points greater than or equal to 50 AND hasSaidMessage is false
     /// print you are awesome!
     /// </summary>
 
     [SerializeField]
     private int _points;
+
+    private bool _hasMessageBeenSent = false;
 
     // Update is called once per frame
     void Update()
@@ -23,9 +26,10 @@ public class MasterIfStatementsP2 : MonoBehaviour
             _points += 10;
         }
 
-        if (_points >= 50)
+        if (_points >= 50 && _hasMessageBeenSent == false)
         {
             Debug.Log("You are awesome!");
+            _hasMessageBeenSent = true;
         }
     }
 }
