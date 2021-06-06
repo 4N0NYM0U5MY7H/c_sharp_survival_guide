98770bccf5d3e9378c6d2603e88fa60909d886df
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 00:21:48 -0700

    Challenge: Convert to For Loop

---
+++ Assets/Scripts/Arrays/LoopingPracticalP2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingPracticalP2 : MonoBehaviour
+{
+    /// <summary>
+    /// Convert the program from a foreach loop to a for loop
+    /// </summary>
+
+    public GameObject[] cubes;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            foreach (var cube in cubes)
+            {
+                cube.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            for (int i = 0; i < cubes.Length; i++)
+            {
+                cubes[i].GetComponent<MeshRenderer>().material.color = Color.green;
+
+            }
+        }
+    }
+}
