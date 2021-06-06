49b9f7d618a7983791bd2162b23dd71914756a62
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 21:20:50 -0700

    For Loop Presentation

---
+++ Assets/Scripts/Loops/ForLoopPresentation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoopPresentation : MonoBehaviour
+{
+    /// <summary>
+    /// create a program that "increments" the number of apples we have
+    /// </summary>
+
+    public int apples;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AddApplesRoutine());
+    }
+
+    IEnumerator AddApplesRoutine()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            apples++;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
