f44fb57fa7ba6497d95097ed6ae67ec6914c198e
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 22:44:25 -0700

    Array Syntax

---
+++ Assets/Scripts/Arrays/ArraySyntax.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraySyntax : MonoBehaviour
+{
+    // declare the size (how many elements) through the inspector
+    public string[] names;
+
+    public string[] items = new string[5];
+
+    public int[] ages = new int[] { 5, 3, 2, 55, 66 };
+}
