7ab59c45a3a9231bb7701d7791d5e4adacf8f517
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 00:21:24 -0700

    Project Clean Up
    more organization throughout the project

--- Assets/Scripts/Arrays/LoopingPractical.cs
+++ Assets/Scripts/Arrays/LoopingPractical.cs
@@ -6,12 +6,6 @@ public class LoopingPractical : MonoBehaviour
 {
     public GameObject[] cubes;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
