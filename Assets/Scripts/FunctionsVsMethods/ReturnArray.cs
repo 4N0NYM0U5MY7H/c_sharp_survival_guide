c7bab2c3d8056b0fc777a38b80ce2af840b0ca0f
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 10:13:41 -0700

    Return Array

---
+++ Assets/Scripts/FunctionsVsMethods/ReturnArray.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnArray : MonoBehaviour
+{
+    public GameObject[] players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = GetAllPlayers();
+    }
+
+   GameObject[] GetAllPlayers()
+    {
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (var p in allPlayers)
+        {
+            p.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+        }
+
+        return allPlayers;
+    }
+}
