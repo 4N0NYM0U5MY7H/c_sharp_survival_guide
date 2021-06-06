3e1111bbcecc89a4070326cb3f4a2a614c4e90e4
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-05 17:04:15 -0700

    If Statements

---
+++ Assets/Scripts/Tutorials/PlayerIfStatements.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIfStatements : MonoBehaviour
+{
+    public int age = 26;
+    private int _legalAgeToDrive = 16;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if age is greater than or euqal to 16 (legal age to drive)
+        // then print you can drive
+        // else
+        // print you can't drive
+
+        if (age >= _legalAgeToDrive)
+        {
+            Debug.Log("You can drive!");
+        }
+        else
+        {
+            Debug.Log("You can't drive!");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
