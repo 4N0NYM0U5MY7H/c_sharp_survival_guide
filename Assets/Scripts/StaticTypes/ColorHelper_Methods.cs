7c5016b29a75db7e87a24c2cd52eadd98cd13a2b
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 22:55:38 -0700

    Challenge: Random Color Helper

---
+++ Assets/Scripts/StaticTypes/ColorHelper_Methods.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ColorHelper_Methods
+{
+    /// <summary>
+    /// create a helper class that passes an object as a parameter and changes the color.
+    ///
+    /// player can activate with the space key
+    ///
+    /// Bonus: randomize the color
+    /// </summary> 
+
+    public static void ChangeColor(GameObject obj, Color color, bool randomColor = false)
+    {
+        if (randomColor == true)
+        {
+            color = new Color(Random.value, Random.value, Random.value);
+        }
+        obj.GetComponent<MeshRenderer>().material.color = color;
+    }
+}
---
+++ Assets/Scripts/StaticTypes/ColorHelper_Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorHelper_Player : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ColorHelper_Methods.ChangeColor(this.gameObject, Color.red);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ColorHelper_Methods.ChangeColor(this.gameObject, Color.red, true);
+        }
+    }
+}
