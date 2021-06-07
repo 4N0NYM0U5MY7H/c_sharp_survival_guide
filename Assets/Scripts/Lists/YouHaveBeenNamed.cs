d0cc16fb4ec1ad70f081608a042b5bad51f99425
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 08:37:29 -0700

    Challenge: You Have Been Named

---
+++ Assets/Scripts/Lists/YouHaveBeenNamed.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.Lists
+{
+    public class YouHaveBeenNamed : MonoBehaviour
+    {
+        /// Challenge:
+        ///     Create a list of names and print each.
+        ///     On key press
+        ///         remove" a random element from the list
+        ///         re-print the remaining names
+        /// 
+
+        public List<string> names = new List<string>();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            foreach (var name in names)
+            {
+                Debug.Log(name);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Delete))
+            {
+                var nameToRemove = names[Random.Range(0, names.Count)];
+
+                names.Remove(nameToRemove);
+
+                foreach (var name in names)
+                {
+                    Debug.Log(name);
+                }
+
+                Debug.Log("We removed " + nameToRemove);
+            }
+        }
+    }
+}
