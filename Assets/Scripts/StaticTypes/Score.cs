a495afa0aab69576ff085a678ae212a47900f79e
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 21:33:23 -0700

    Working with Static Types

---
+++ Assets/Scripts/StaticTypes/Score.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public static int score;
+
+    public static void AddScore()
+    {
+        // do stuff
+    }
+}
---
+++ Assets/Scripts/StaticTypes/StaticTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticTest : MonoBehaviour
+{
+    private Score _score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _score = GameObject.Find("Score Keeper").GetComponent<Score>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // _score.score += 10;
+            Score.score += 10;
+
+            Debug.Log("Current Score: " + Score.score);
+        }
+    }
+}
