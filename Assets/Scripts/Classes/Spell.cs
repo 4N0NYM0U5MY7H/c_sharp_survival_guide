a662d7eaa629bc59a4568fc5305437ed6c25c919
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 18:37:24 -0700

    RPG Spell System

---
+++ Assets/Scripts/Classes/Spell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Spell
+{
+    public string name;
+    public int levelRequired;
+    public int itemIdRequired;
+    public int expGained;
+
+    public Spell(string name, int levRequired, int itemIdRequired, int exp)
+    {
+        this.name = name;
+        this.levelRequired = levRequired;
+        this.itemIdRequired = itemIdRequired;
+        this.expGained = exp;
+    }
+
+    public void Cast()
+    {
+        Debug.Log("Casting: " + this.name);
+    }
+}
---
+++ Assets/Scripts/Classes/Wizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : MonoBehaviour
+{
+    public Spell fireBall;
+
+    public Spell[] spells;
+
+    public int level = 1;
+    public int exp;
+
+    private void Start()
+    {
+        fireBall = new Spell("Fire Ball", 1, 17, 35);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            fireBall.Cast();
+            exp += fireBall.expGained;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var spell in spells)
+            {
+                if (spell.levelRequired == level)
+                {
+                    spell.Cast();
+                    exp += spell.expGained;
+                }
+            }
+        }
+    }
+    
+}
