6601269dd78a02447c33c320c6795a9c4d857658
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 07:32:29 -0700

    Auto Properties

---
+++ Assets/Scripts/Properties/AutoProperties_GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoProperties_GameManager : MonoBehaviour
+{
+    public bool IsGameOver { get; protected set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsGameOver = false;
+
+        if (IsGameOver)
+        {
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IsGameOver = true;
+        }
+    }
+}
