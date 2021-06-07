5d4588819a0944705652627de9727ab829f1bd39
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 07:52:28 -0700

    Challenge: Declaring Properties

---
+++ Assets/Scripts/Properties/PropertiesChallenge_GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertiesChallenge_GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// Create properties for the following:
+    ///     Speed (read only)
+    ///     Name (public)
+    /// </summary>
+
+    public float Speed { get; private set; }
+    public string Name { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = 10f;
+        Debug.Log(Speed);
+    }
+}
