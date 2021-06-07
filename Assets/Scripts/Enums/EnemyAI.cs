53b2d32cbd0fa9ff50266622b6918f57d81c55de
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 13:40:11 -0700

    Enum Enemy AI

---
+++ Assets/Scripts/Enums/EnemyAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.Enums
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        public enum EnemyState
+        {
+            Patrolling,
+            Attacking,
+            Pursuing,
+            Death
+        }
+
+        public EnemyState currentState;
+
+        // Update is called once per frame
+        void Update()
+        {
+            switch (currentState)
+            {
+                case EnemyState.Patrolling:
+                    Debug.Log("Patrolling");
+                    if (Time.time > 5)
+                    {
+                        currentState = EnemyState.Pursuing;
+                    }
+                    break;
+                case EnemyState.Attacking:
+                    Debug.Log("Attacking");
+                    break;
+                case EnemyState.Pursuing:
+                    Debug.Log("Pursuing");
+                    break;
+                case EnemyState.Death:
+                    Debug.Log("Death");
+                    break;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                currentState = EnemyState.Attacking;
+            }
+        }
+    }
+}
+
