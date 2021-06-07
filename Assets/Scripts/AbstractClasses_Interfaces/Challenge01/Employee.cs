bd08de28d887492e67684307fd89f45afdf83287
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 16:54:30 -0700

    Challenge: Employee Experience

---
+++ Assets/Scripts/AbstractClasses_Interfaces/Challenge01/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.AbstractClass_Interface.Challenge01
+{
+    /// Challenge:
+    ///     create an abstract Employee class that defines the following traits:
+    ///     Company Name
+    ///     Employee Name
+    ///     CalculateMonthlySalary()
+    ///
+    /// FullTime : Employee
+    /// salary
+    /// FORCE implement CalculateMonthlySalary()
+    /// 
+    /// PartTime : Employee
+    /// hoursWorked
+    /// hourlyRate
+    /// FORCE implement CalculateMonthlySlary()
+
+    public abstract class Employee : MonoBehaviour
+    {
+        public static string company = "GameDevHQ";
+        public string employeeName;
+
+        public abstract void CalculateMonthlySalary();
+    }
+}
---
+++ Assets/Scripts/AbstractClasses_Interfaces/Challenge01/FullTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.AbstractClass_Interface.Challenge01
+{
+    public class FullTime : Employee
+    {
+        public int salary;
+
+        public override void CalculateMonthlySalary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
