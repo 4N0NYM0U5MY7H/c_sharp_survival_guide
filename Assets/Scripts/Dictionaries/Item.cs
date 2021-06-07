a742228db0e3ad61b82df73785e1c601bb966429
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 14:43:49 -0700

    Dictionaries

---
+++ Assets/Scripts/Dictionaries/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.Dictionaries 
+{
+    [System.Serializable]
+    public class Item
+    {
+        public string name;
+        public int ID;
+    }
+}
