0c9861a464418b60e32c79184c656df6b592bdb3
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 13:23:57 -0700

    Enums

---
+++ Assets/Scripts/Enums/SelectDifficulty.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.Enums 
+{
+    public class SelectDifficulty : MonoBehaviour
+    {
+        public enum LevelSelector
+        {
+            Easy, // 0
+            Normal, // 1
+            Hard, // 2
+            Expert // 3
+        }
+
+        public LevelSelector currentLevel;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            switch (currentLevel)
+            {
+                case LevelSelector.Easy:
+                    Debug.Log("Easy Difficulty");
+                    break;
+                case LevelSelector.Normal:
+                    Debug.Log("Normal Difficulty");
+                    break;
+                case LevelSelector.Hard:
+                    Debug.Log("Hard Difficulty");
+                    break;
+                case LevelSelector.Expert:
+                    Debug.Log("Expert Difficulty");
+                    break;
+                default:
+                    Debug.Log("Something Broke!");
+                    break;
+            }
+        }
+    }
+}
