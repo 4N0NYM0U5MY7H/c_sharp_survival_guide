9f54f7d92e72e69b182ccebcffd771c468f5f452
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 18:10:16 -0700

    Challenge: Customer Database

---
+++ Assets/Scripts/Classes/CustomerDataBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerDataBase : MonoBehaviour
+{
+    /// <summary>
+    /// Create a program which adds "Customers" to a database.
+    /// Assume the database needs customer information.
+    ///
+    /// Create at least 3 customers via code using a constructor.
+    /// Should also be able to add customers through the inspector.
+    ///
+    /// Customer Traits:
+	///     first name
+	///     last name
+	///     age
+	///     gender
+	///     occupation
+    /// </summary>
+
+    Customer jonathan;
+    Customer jannet;
+    Customer jessica;
+    Customer eric;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jonathan = new Customer("Jonathan", "Wienberger", 26, "M", "Software Engineer");
+        jannet = new Customer("Jannet", "", 55, "F", "Instructor");
+        jessica = new Customer("Jessica", "Lang", 23, "F", "Scientist");
+    }
+}
--- Assets/Scripts/Classes/RPGItemDataBase.cs
+++ Assets/Scripts/Classes/RPGItemDataBase.cs
@@ -32,6 +32,4 @@ public class RPGItemDataBase : MonoBehaviour
 
         return item;
     }
-
-
 }
