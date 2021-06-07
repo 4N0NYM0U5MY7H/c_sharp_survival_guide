918d08fb507fa630b3794cdb313c0a618823cbd1
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 22:37:24 -0700

    Utility Helper Classes

---
+++ Assets/Scripts/StaticTypes/StaticPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticPlayer : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UtilityHelper.CreateGameObject();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            UtilityHelper.SetPositionToZero(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/StaticTypes/UtilityHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilityHelper
+{
+    public static void CreateGameObject()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+
+    public static void SetPositionToZero(GameObject obj)
+    {
+        obj.transform.position = Vector3.zero;
+    }
+}
