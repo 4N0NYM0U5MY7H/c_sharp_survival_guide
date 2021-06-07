cc2f39a4386dd4eae7f7221007b87937cebf14c7
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 13:54:53 -0700

    Enums in Cutom Classes

---
+++ Assets/Scripts/Enums/Item.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.Enums 
+{
+    [System.Serializable]
+    public class Item
+    {
+        public string name;
+        public int ID;
+        public Sprite icon;
+
+        public enum ItemType
+        {
+            None,
+            Weapon,
+            Consumable
+        }
+
+        public ItemType itemType;
+
+        public void Action()
+        {
+            switch (itemType)
+            {
+                case ItemType.Weapon:
+                    Debug.Log("This is a " + itemType);
+                    break;
+                case ItemType.Consumable:
+                    Debug.Log("This is a " + itemType);
+                    break;
+            }
+        }
+    }
+}
+
