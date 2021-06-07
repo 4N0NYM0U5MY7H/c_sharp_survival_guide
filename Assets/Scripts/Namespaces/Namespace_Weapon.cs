daf66d463807c6cc46095ede2ebeae0e41248d9c
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 08:05:33 -0700

    Namespaces
    Note to self:  use namespaces to clean up project at the end

---
+++ Assets/Scripts/Namespaces/Namespace_Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace WeaponPack.Magic 
+{
+    public class Namespace_Weapon : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Image. // UI stuff
+            // SceneManager. // load scences
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
