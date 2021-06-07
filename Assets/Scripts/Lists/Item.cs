3ffa9d81a1b712a20a88974434c065da19d2dac1
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-07 13:08:35 -0700

    Item DB System Presentation

---
+++ Assets/Scripts/Lists/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Section.Lists;
+
+namespace Section.Lists
+{
+    [System.Serializable]
+    public class Item
+    {
+        public string name;
+        public int id;
+        public Sprite icon;
+    }
+}
---
+++ Assets/Scripts/Lists/ItemDB.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Section.Lists;
+
+namespace Section.Lists
+{
+    public class ItemDB : MonoBehaviour
+    {
+        public List<Item> itemDatabase = new List<Item>();
+
+        public void AddItem(int itemID, Player _player)
+        {
+            // check if item matches something in the database
+            foreach (var item in itemDatabase)
+            {
+                // check for match
+                if (item.id == itemID)
+                {
+                    Debug.Log("We have a match!");
+
+                    // check for available inventory slots
+                    _player.inventory[0] = item;
+                    return;
+                }
+            }
+
+            Debug.Log("That item does not exist");
+        }
+
+        public void RemoveItem(int itemID, Player _player)
+        {
+            foreach (var item in itemDatabase)
+            {
+                // check for match
+                if (item.id == itemID)
+                {
+                    Debug.Log("We have a match!");
+
+                    // check for available inventory slots
+                    _player.inventory[0] = null; 
+                }
+            }
+        }
+    }
+}
+
