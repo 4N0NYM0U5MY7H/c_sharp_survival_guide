30cbcc141b7a6f2e590babbce154545f979a49c0
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-06 19:15:35 -0700

    Virtual Methods and Overriding

---
+++ Assets/Scripts/Classes/Dog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : Pet
+{
+    protected override void Speak()
+    {
+        Debug.Log("Bark!");
+    }
+}
---
+++ Assets/Scripts/Classes/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    protected string petName;
+
+    protected virtual void Speak()
+    {
+        Debug.Log("Speak!");
+    }
+
+    private void Start()
+    {
+        Speak();
+    }
+}
