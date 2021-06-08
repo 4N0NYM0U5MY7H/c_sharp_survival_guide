1c37d41dacc1ee0af9b206c78b3ca8f5c37ea90b
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-08 14:45:24 -0700

    Practice Delegates of Type Void with NO Parameters

---
+++ Assets/Scripts/Delegates_Events/PracticeDelegate02/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Section.Delegates_Events.PracticeDelegate02
+{
+    public class Main : MonoBehaviour
+    {
+        /// Creata a delegate of type void that has no parameter and returns the gameObject's name
+        public Action onGetName;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            onGetName = () => 
+            {
+                Debug.Log("Name: " + gameObject.name);
+                Debug.Log("It's a nice day out!");
+            };
+
+            // code
+            // code
+
+            onGetName();
+        }
+    }
+}
