1d7fdc84d588182ce4029340f1fffaed736fd8d0
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-08 11:44:57 -0700

    Working with C# Delegates and Events

---
+++ Assets/Scripts/Delegates_Events/Delegates_Events/Sphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.Delegates_Events.Delegates_Events
+{
+    public class Sphere : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Main.onClick += Fall;
+        }
+
+        public void Fall()
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
