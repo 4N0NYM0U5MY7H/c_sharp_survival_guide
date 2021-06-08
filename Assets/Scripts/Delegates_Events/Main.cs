bfd479062b552ad8c9fe4e97c9457ce3d9bf9a93
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-08 10:11:32 -0700

    Delegates and Events

---
+++ Assets/Scripts/Delegates_Events/Main.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.Delegates_Events
+{
+    public class Main : MonoBehaviour
+    {
+        public delegate void ChangeColor(Color newColor);
+        public ChangeColor onColorChange;
+
+        public delegate void OnComplete();
+        public OnComplete onComplete;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            onColorChange = UpdateColor;
+            onColorChange(Color.green);
+
+            // signitures must match
+            // onColorChange = Task;
+            
+            onComplete += Task;
+            onComplete += Task2;
+            onComplete += Task3;
+
+            onComplete -= Task3;
+
+            if (onComplete != null)
+            {
+                onComplete();
+            }
+        }
+
+        public void UpdateColor(Color newColor)
+        {
+            Debug.Log("Changing color to: " + newColor.ToString());
+        }
+
+        public void Task()
+        {
+            Debug.Log("Task 1 Finished");
+        }
+
+        public void Task2()
+        {
+            Debug.Log("Task 2 Finished");
+        }
+
+        public void Task3()
+        {
+            Debug.Log("Task 3 Finished");
+        }
+    }
+}
