a9e449ebf940eca12eb98bd3030a70afb3fe2783
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-08 11:58:55 -0700

    Challenge: Teleport Events

---
+++ Assets/Scripts/Delegates_Events/Challenge01/Cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.Delegates_Events.Challenge01
+{
+    public class Cube : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Main.onTeleport += Spawn;
+        }
+
+        public void Spawn(Vector3 pos)
+        {
+            transform.position = pos;
+        }
+
+        private void OnDisable()
+        {
+            Main.onTeleport -= Spawn;
+        }
+    }
+}
---
+++ Assets/Scripts/Delegates_Events/Challenge01/Main.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.Delegates_Events.Challenge01
+{
+    public class Main : MonoBehaviour
+    {
+        /// Challenge:
+        /// Create a program using delegates and events:
+        ///     On key press, assign a Cube the postion of (5, 2, 0)
+        ///     The Cube script should subsribe to the event and be invoked on key press
+        ///     
+
+
+        public delegate void Teleport(Vector3 pos);
+        public static event Teleport onTeleport;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (onTeleport != null)
+                {
+                    Vector3 pos = new Vector3(5, 2, 0);
+                    onTeleport(pos);
+                }
+            }
+        }
+    }
+}
