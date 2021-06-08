416fc7ef828ac72a357d1f8f3d3082543b49a8bf
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-08 15:38:58 -0700

    Simple Callback System

---
+++ Assets/Scripts/Delegates_Events/CallbackSystem/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Section.Delegates_Events.CallbackSystem
+{
+    public class Main : MonoBehaviour
+    {
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartCoroutine(MyRoutine(() =>
+            {
+                Debug.Log("The Routine has finished!");
+                Debug.Log("Wow this is cool!");
+            }));
+        }
+
+        public IEnumerator MyRoutine(Action onComplete = null)
+        {
+            yield return new WaitForSeconds(5.0f);
+
+            if (onComplete != null)
+                onComplete();
+        }
+    }
+}
