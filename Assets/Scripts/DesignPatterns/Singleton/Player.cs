f34f1a4dedb4d93e8fadd2bb513f498fdd219fa8
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-09 14:06:25 -0700

    Design Patterns: Singleton

---
+++ Assets/Scripts/DesignPatterns/Singleton/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Section.DesignPatterns.Singleton
+{
+    public class Player : MonoBehaviour
+    {
+        // Traditional method
+        //private GameManager _gm;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Traditoinal method
+            //_gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+            GameManager.Instance.DisplayName();
+        }
+    }
+}
