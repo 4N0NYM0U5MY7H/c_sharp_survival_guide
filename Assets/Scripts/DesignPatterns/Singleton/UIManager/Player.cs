b05e3921fd8a14770576f74e9214ac4d0b4b5c68
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-09 14:23:23 -0700

    Singleton: UI Manager

---
+++ Assets/Scripts/DesignPatterns/Singleton/UIManager/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Section.DesignPatterns.Singleton.UIManger
+{
+    public class Player : MonoBehaviour
+    {
+        // Traditional method
+        //private UIManager _ui;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Traditoinal method
+            //_ui = GameObject.Find("UIManager").GetComponent<UIManager>();
+            //_ui.UpdateScore(40);
+
+            UIManager.Instance.UpdateScore(40);
+        }
+    }
+}
