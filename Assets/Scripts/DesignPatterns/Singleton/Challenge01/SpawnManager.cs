8455ca82dbdfb8e88dac9d4bd71ada056dc9d0b4
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-09 14:36:45 -0700

    Challenge: Singleton Spawn Manager

---
+++ Assets/Scripts/DesignPatterns/Singleton/Challenge01/SpawnManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.DesignPatterns.Singleton.Challenge01
+{
+    public class SpawnManager : MonoBehaviour
+    {
+        /// Challenge:
+        /// Create the instance (private)
+        /// Check if the instance is null
+        /// Error handle
+        /// Assign the instance (void Awake)
+        /// 
+
+        private static SpawnManager _instance;
+        public static SpawnManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    Debug.LogError("SpawnManager is NULL.");
+
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            _instance = this;
+        }
+
+        public void StartSpawning()
+        {
+            Debug.Log("Spawn Started");
+        }
+    }
+}
