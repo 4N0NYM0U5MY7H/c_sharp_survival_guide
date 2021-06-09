97e5a2eb35bf53b38efcd87619d1eee425e7be07
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-09 13:16:57 -0700

    Challenge: Filter Items with LINQ

---
+++ Assets/Scripts/LINQ/Challenge02/LinqExample.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Section.LINQ.Challenge02
+{
+    [System.Serializable]
+    public class Item
+    {
+        public string name;
+        public int itemID;
+        public int buff;
+    }
+
+    public class LinqExample : MonoBehaviour
+    {
+        /// Challenge:
+        /// Check if itemID 3 exists in the list and print out true or false.
+        /// Grab all items with buff greater than 20 and print them out.
+        /// Calculate the average of all the buff stats.
+        /// 
+
+        public List<Item> items;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            var itemExists = items.Any(item => item.itemID == 3);
+            Debug.Log("ItemID 3 Exists: " + itemExists);
+
+            var itemBuff = items.Where(item => item.buff > 20);
+            foreach (var item in itemBuff)
+            {
+                Debug.Log("Item with Buff greater than 20: " + item.name);
+            }
+
+            var buffAvg = items.Average(item => item.buff);
+            Debug.Log("Buff Average: " + buffAvg);
+        }
+    }
+}
--- Assets/Scripts/LINQ/DecendingOrder/LinqExample.cs
+++ Assets/Scripts/LINQ/DecendingOrder/LinqExample.cs
@@ -5,12 +5,6 @@ namespace Section.LINQ.DecendingOrder
 {
     public class LinqExample : MonoBehaviour
     {
-        /// Challenge:
-        /// Create a program where you have an integer array of quiz grades.
-        /// The quiz grades should be between 0-100.
-        /// Filter through the quiz grades and create a new collection of only passing grades (above 69)
-        /// 
-
         public int[] quizGrades = { 44, 55, 32, 77, 95, 98, 99 };
 
         // Start is called before the first frame update
