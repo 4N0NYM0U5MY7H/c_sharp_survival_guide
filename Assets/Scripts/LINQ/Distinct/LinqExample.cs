acbc62e8cd624b069529eb9d2423d4967d944f2a
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-09 10:01:23 -0700

    LINQ Distinct

---
+++ Assets/Scripts/LINQ/Distinct/LinqExample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Linq;
+
+namespace Section.LINQ.Distinct
+{
+    public class LinqExample : MonoBehaviour
+    {
+        public string[] names = { "jon", "jon", "alex", "julie", "alex", "julie", "jessie", "david", "mark", "matt" };
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            var uniqueNames = names.Distinct();
+
+            foreach (var name in uniqueNames)
+            {
+                Debug.Log("Name: " + name);
+            }
+        }
+    }
+}
