f08668848c2ad75a0a02af6d8f62e10c406d35cd
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-09 18:08:33 -0700

    Challenge: Pool Manager

---
+++ Assets/Scripts/DesignPatterns/ObjectPooling/Challenge 01/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Section.DesignPatterns.ObjectPooling.Challenge01
+{
+    public class Bullet : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Destroy(this.gameObject, 1f); // recycle
+        }
+    }
+}
---
+++ Assets/Scripts/DesignPatterns/ObjectPooling/Challenge 01/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Section.DesignPatterns.ObjectPooling.Challenge01
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _bulletPrefab;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(_bulletPrefab); // re-use
+            }
+        }
+    }
+}
