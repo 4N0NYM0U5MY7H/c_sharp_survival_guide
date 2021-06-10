6b5fabc71128a066a9be90d807ea319653acf5eb
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-09 20:52:00 -0700

    Implementing the Command Pattern

---
+++ Assets/Scripts/DesignPatterns/CommandPattern/Commands/ClickCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.DesignPatterns.Command
+{
+    public class ClickCommand : ICommand
+    {
+        private GameObject _cube;
+        private Color _color;
+        private Color _previousColor;
+
+        public ClickCommand(GameObject cube, Color color)
+        {
+            this._cube = cube;
+            this._color = color;
+        }
+
+        public void Execute()
+        {
+            _previousColor = _cube.GetComponent<MeshRenderer>().material.color;
+            _cube.GetComponent<MeshRenderer>().material.color = _color;
+        }
+
+        public void Undo()
+        {
+            _cube.GetComponent<MeshRenderer>().material.color = _previousColor;
+        }
+    }
+}
---
+++ Assets/Scripts/DesignPatterns/CommandPattern/Interfaces/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section.DesignPatterns.Command
+{
+    public interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+}
