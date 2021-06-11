87e9683d06a2c95e7db3ea03db59d845d0353b56
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-11 13:06:44 -0700

    Project Organization: Properties
    Organized all scenes and files related to the Properties section of the course

---
+++ Assets/Scripts/09_Properties/AutoProperties/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Section.Properties.Auto
+{
+    public class GameManager : MonoBehaviour
+    {
+        public bool IsGameOver { get; protected set; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            IsGameOver = false;
+
+            if (IsGameOver)
+            {
+
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                IsGameOver = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/09_Properties/Challenge01/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Section.Properties.Challenge01
+{
+    public class GameManager : MonoBehaviour
+    {
+        /// Challenge Overview:
+        /// Create properties for the following:
+        ///     Speed (read only)
+        ///     Name (public)
+        /// 
+
+        public float Speed { get; private set; }
+        public string Name { get; set; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Speed = 10f;
+            Debug.Log(Speed);
+        }
+    }
+}
