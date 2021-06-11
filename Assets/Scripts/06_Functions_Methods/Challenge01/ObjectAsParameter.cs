502e125c14b3d6c99ab07d4815afc09abfe12682
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2021-06-10 17:12:42 -0700

    Project Organization: Functions vs Methods
    Cleaned up all scenes and files in the Functions vs Methods section of the course

---
+++ Assets/Scripts/06_Functions_Methods/Challenge01/ObjectAsParameter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Section.Methods.Challenge01
+{
+    public class ObjectAsParameter : MonoBehaviour
+    {
+        /// Challenge Overview:
+        /// Create a program that takes an assigned game object from the inspector and pass
+        /// that object as a parameter for the method ChangeColor
+        /// The parameters for the method are the GameObject and the desired color
+        /// All operations should be performed within one method.
+        /// 
+
+        public GameObject cube;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ChangeColor(cube, Color.green);
+            }
+        }
+
+        void ChangeColor(GameObject obj, Color colorToAssign)
+        {
+            obj.GetComponent<MeshRenderer>().material.color = colorToAssign;
+        }
+    }
+}
---
+++ Assets/Scripts/06_Functions_Methods/Parameters.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Section.Methods
+{
+    public class Parameters : MonoBehaviour
+    {
+        public int health;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Damage(5);
+            }
+        }
+
+        public void Damage(int damageAmount)
+        {
+            health -= damageAmount;
+
+            if (health < 1)
+            {
+                health = 0;
+                Destroy(this.gameObject);
+            }
+        }
+
+        void Sum(int a, int b)
+        {
+            Debug.Log(a + b);
+        }
+
+        private void Attack()
+        {
+            // attack related code
+        }
+
+        private void Powerup()
+        {
+            // powerup related code
+        }
+
+        private void Shoot()
+        {
+            // shoot related code
+        }
+    }
+}
